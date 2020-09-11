2589fc4b0a41458d75357811bed278b7d7b85da9
Tomáš Raček <tom@krab1k.net>
2020-09-11 16:56:24 +0200

    [create_binary_archive] Allow the process to be re-ran using existing log

--- distance/create_binary_archive.cpp
+++ distance/create_binary_archive.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <algorithm>
 #include <set>
+#include <fstream>
 #include <omp.h>
 
 #include "gesamtlib/gsmt_structure.h"
@@ -18,18 +19,40 @@ std::string to_upper(std::string s) {
 
 int main(int argc, char **argv) {
 
-    if (argc < 3) {
+    if (argc < 4) {
         std::cerr << "Not enough arguments" << std::endl;
         return 1;
     }
 
     const fs::path raw_pdb_dir = argv[1];
     const fs::path output_binary_dir = argv[2];
+    const fs::path logfile = argv[3];
+
+    std::ifstream log_read;
+    log_read.open(logfile);
+
+    std::set<std::string> processed;
+
+    std::string line;
+    while (std::getline(log_read, line)) {
+        auto found = line.find("Done reading: ");
+        if (found != std::string::npos) {
+            auto filename = line.substr(14);
+            processed.insert(filename);
+        }
+    }
+    log_read.close();
+
+    std::ofstream log(logfile, std::fstream::app | std::fstream::out);
 
     std::vector<fs::directory_entry> files;
     std::set<std::string> subdirs;
     for (const auto &file_entry: fs::directory_iterator(raw_pdb_dir)) {
         if (not file_entry.is_regular_file()) {
+            continue;
+        }
+
+        if (processed.find(std::string(file_entry.path())) != processed.end()) {
             continue;
         }
 
@@ -41,12 +64,17 @@ int main(int argc, char **argv) {
         fs::create_directory(output_binary_dir / subdir);
     }
 
-#pragma omp parallel for default(none) shared(files, output_binary_dir, std::cerr, std::cout)
+
+#pragma omp parallel for default(none) shared(files, output_binary_dir, log, processed)
     for (size_t i = 0; i < files.size(); i++) {
         const auto &file_entry = files[i];
 
-        std::string pdb_id = to_upper(std::string(file_entry.path().filename()).substr(0, 4));
+        const std::string pdb_id = to_upper(std::string(file_entry.path().filename()).substr(0, 4));
         const std::string subdir = std::string(file_entry.path().filename()).substr(0, 2);
+
+        std::stringstream ss;
+        ss << "Started reading: " << std::string(file_entry.path()) << std::endl;
+        log << ss.str();
 
         int chain_no = 0;
         int rc;
@@ -54,12 +82,15 @@ int main(int argc, char **argv) {
             gsmt::Structure structure;
             rc = structure.getStructure(file_entry.path().c_str(), nullptr, chain_no, true);
             if (rc && chain_no) {
+                std::stringstream ss1;
+                ss1 << "Done reading: " << std::string(file_entry.path()) << std::endl;
+                log << ss1.str();
                 break;
             }
             if (rc) {
-                std::stringstream ss;
-                ss << "Cannot read from file: " << std::string(file_entry.path()) << std::endl;
-                std::cerr << ss.str();
+                std::stringstream ss1;
+                ss1 << "Cannot read from file: " << std::string(file_entry.path()) << std::endl;
+                log << ss1.str();
                 break;
             }
 
@@ -73,16 +104,21 @@ int main(int argc, char **argv) {
                 std::stringstream ss1, ss2;
 
                 ss1 << "Converted: " << pdb_id << ":" << chain_id << std::endl;
-                std::cout << ss1.str();
+                log << ss1.str();
                 ss2 << std::string(output_binary_dir) << "/" << subdir << "/" << pdb_id << ":" << chain_id << ".bin";
                 file.assign(ss2.str().c_str());
                 file.rewrite();
                 structure.write(file);
                 file.shut();
+            } else if (n_atoms > 0) {
+                std::stringstream ss1;
+                ss1 << "Chain too short: " << pdb_id << ":" << atom[0]->GetChainID() << std::endl;
+                log << ss1.str();
             }
             chain_no++;
         }
-
     }
+
+    log.close();
     return 0;
 }
