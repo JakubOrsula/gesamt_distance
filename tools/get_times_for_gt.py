aa61ba59efac1f2a2273f77d09544e8a11515a20
Tomáš Raček <tom@krab1k.net>
2020-10-30 18:41:52 +0100

    [tools] Calculate time for ground truth

---
+++ tools/get_times_for_gt.py
@@ -0,0 +1,37 @@
+import json
+import csv
+import sys
+import python_distance
+import time
+
+N = 30
+
+if __name__ == '__main__':
+
+    archive_dir = sys.argv[1]
+    mapping_file = sys.argv[2]
+    gt_file = sys.argv[3]
+
+    to_compute = {}
+    mapping = {}
+    with open(mapping_file) as f:
+        reader = csv.reader(f, delimiter=';')
+        for chain_id, number in reader:
+            mapping[number] = chain_id
+
+    with open(gt_file) as f:
+        for line in f:
+            record = json.loads(line)
+            query_number = record['queryObj_id']
+            similar_objects = [mapping[nr['_id']] for nr in record['answer_records'][:N]]
+            to_compute[mapping[query_number]] = similar_objects
+
+    python_distance.init_library(archive_dir, '/dev/null', True, 0, 100)
+    for query, nearest in to_compute.items():
+        if not query.startswith('1'):
+            continue
+        start = time.time()
+        for obj in nearest:
+            python_distance.get_distance(query, obj, -1)
+        end = time.time()
+        print(f'{query} {end - start}')
